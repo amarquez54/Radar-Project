ce652a44efb81085d9b4feac755d596cbb02a384
Alejandra <amarquez54@miners.utep.edu>
2026-04-06 19:09:03 -0600

    distance radar script

---
+++ distance_radar.py
@@ -0,0 +1,62 @@
+from acconeer.exptool import a121
+from acconeer.exptool.a121.algo.distance import Detector as DistanceDetector, DetectorConfig as DistanceConfig
+import json
+
+client = a121.Client.open(serial_port="/dev/ttyUSB0")
+
+tracked_distance = None
+threshold = 0.5
+miss_count = 0
+max_misses = 2
+
+distance_detector = DistanceDetector (
+    client = client,
+    sensor_ids=[1],
+    detector_config = DistanceConfig(
+        start_m = 0.2, #you can change these parameters
+        end_m = 1.5
+    )
+)
+
+distance_detector.calibrate_detector()
+distance_detector.start()
+
+try:
+    while True:
+        result = distance_detector.get_next()[1]
+        distances = result.distances
+
+        if len(distances)==0:
+            miss_count+=1
+
+        else:
+            measured_distance = min(distances)
+            
+            if tracked_distance is None or abs(measured_distance - tracked_distance) <= threshold:
+                tracked_distance = measured_distance
+                miss_count = 0
+            else:
+                miss_count+=1
+
+        if miss_count > max_misses: 
+                tracked_distance = None 
+                miss_count = 0
+
+        valid = tracked_distance is not None
+            
+        if tracked_distance is None:
+            data = {"distance": None}
+            
+        else:
+            data = {
+                "distance": round(tracked_distance, 3),
+                "valid": valid
+            }
+
+        print(json.dumps(data))
+
+except KeyboardInterrupt:
+    print("Stopping")
+
+distance_detector.stop()
+client.close()

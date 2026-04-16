ac744557f4f9219aac6020a29e316892db809ebc
amarquez54 <amarquez54@miners.utep.edu>
2026-04-16 17:03:03 -0600

    changes

--- tracking_system.py
+++ tracking_system.py
@@ -4,12 +4,17 @@ import time
 
 client = a121.Client.open(serial_port="/dev/ttyUSB0")
 
-previous_reading = None
+tracking_reading = None
 threshold = 0.5
 miss_count = 0
 max_misses = 5
 current_time = time.time() #start timer
-previous_time = 0 # keep track of previous time for speed calculation
+previous_time = 0 
+calculated_speed = 0
+
+def compute_speed(current_reading, tracking_reading, dt):
+        
+        return (current_reading - tracking_reading) / dt
 
 distance_detector = DistanceDetector (
     client = client,
@@ -22,7 +27,6 @@ distance_detector = DistanceDetector (
 
 distance_detector.calibrate_detector()
 distance_detector.start()
-
 
 try:
     while True:
@@ -39,39 +43,45 @@ try:
             current_reading = min(distances) 
 
             # check if there is a value in previous reading, if not, set it to current reading and reset miss count
-            if previous_reading is None:
-                previous_reading = current_reading
+            if tracking_reading is None and previous_time == 0:
+                tracking_reading = current_reading
+                previous_time = current_time
                 miss_count = 0 # restart miss count if we have a valid reading
 
             #if there is a value, compare it to the threshold
-            elif abs(current_reading - previous_reading) <= threshold:
-                if (current_reading - previous_reading) < 0:
+            elif abs(current_reading - tracking_reading) <= threshold:
+                
+                if (current_reading - tracking_reading) < 0:
                      print("Object is approaching")
-                elif (current_reading - previous_reading) > 0:
-                     print("Object is moving away")
-                previous_reading = current_reading
+                     calculated_speed = compute_speed(current_reading, tracking_reading, current_time - previous_time)
+                     print(f"Calculated speed: {calculated_speed:.3f} m/s")
+                     previous_time = current_time # update the previous time for the next speed calculation
+                
+                elif (current_reading - tracking_reading) > 0:
+                    print("Object is moving away")
+                    calculated_speed = compute_speed(current_reading, tracking_reading, current_time - previous_time)
+                    print(f"Calculated speed: {calculated_speed:.3f} m/s")
+                    previous_time = current_time # update the previous time for the next speed calculation
+
+                tracking_reading = current_reading
                 miss_count = 0
+        
             else:
                 miss_count+=1 # otherwise, increase the miss count
 
-
         # if too many miss counts, object lost
         if miss_count > max_misses: 
-                previous_reading = None 
+                tracking_reading = None 
                 miss_count = 0
-                
-        if previous_reading is None:
-            print("None")
-                
+        if tracking_reading is None:
+                print("None")
+                    
         else:
-            print(f"{previous_reading:.3f}")
+            print(f"{tracking_reading:.3f}")
+                
 
 except KeyboardInterrupt:
     print("Stopping tracking system...")
 
     distance_detector.stop()
     client.close()
-
-def compute_speed(current_reading, previous_reading, dt):
-        
-        return (current_reading - previous_reading) / dt
